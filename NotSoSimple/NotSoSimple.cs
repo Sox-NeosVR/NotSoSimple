189651e1fde6ab1e582f2bcd00df209efd205c1e
Sox <66733654+Sox-NeosVR@users.noreply.github.com>
2022-08-26 09:59:55 +0100

    Initial Release
    Release

---
+++ NotSoSimple/NotSoSimple.cs
@@ -0,0 +1,118 @@
+﻿using BaseX;
+using FrooxEngine;
+using FrooxEngine.CommonAvatar;
+using HarmonyLib;
+using NeosModLoader;
+using System.Collections.Generic;
+
+namespace NotSoSimple
+{
+    public class Patch : NeosMod
+    {
+        public override string Name => "NotSoSimple";
+        public override string Author => "Sox & LeCloutPanda";
+        public override string Version => "1.0.0";
+
+        public static ModConfiguration config;
+        private static List<CloudUserRef> users = new List<CloudUserRef>();
+
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<bool> Savent = new ModConfigurationKey<bool>("Savent", "Prevent others being able to save protected items.", () => true);
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<bool> Equipnt = new ModConfigurationKey<bool>("Equipnt", "Prevent others from being able to equip protected avatars.", () => true);
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<bool> Grabnt = new ModConfigurationKey<bool>("Grabnt", "Prevent others from being able to grab protected items.", () => true);
+
+        public override void OnEngineInit()
+        {
+            config = GetConfiguration();
+            config.Save(true);
+
+            Harmony harmony = new Harmony($"dev.{Author}.{Name}");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(SimpleAvatarProtection))]
+        class PatchSimpleAvatarProtection
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("OnAwake")]
+            static void OnAwake(SimpleAvatarProtection __instance, CloudUserRef ___User)
+            {
+                __instance.RunInUpdates(3, () =>
+                {
+                    if (___User.UserId == __instance.LocalUser.UserID)
+                    {
+                        users.Add(___User);
+                        Slot slot = __instance.Slot;
+                        AddProtections(__instance.Slot);
+                    }
+                });
+
+            }
+        }
+
+        static void AddProtections(Slot slot)
+        {
+            if (config.GetValue(Savent)) slot.AttachComponent<ChildrenSaveBlocker>().Persistent = false;
+            if (config.GetValue(Equipnt))
+            {
+                var Ligma = slot.AttachComponent<ValueUserOverride<bool>>();
+                Ligma.Persistent = false;
+                var Balls = slot.AttachComponent<AvatarEquipBlock>();
+                Balls.Persistent = false;
+                Ligma.Target.Value = Balls.EnabledField.ReferenceID;
+                Ligma.Default.Value = true;
+                Ligma.SetOverride(slot.LocalUser, false);
+            }
+            if (config.GetValue(Grabnt))
+            {
+                var Ligma = slot.AttachComponent<ValueUserOverride<bool>>();
+                Ligma.Persistent = false;
+                var Balls = slot.AttachComponent<GrabBlock>();
+                Balls.Persistent = false;
+                Ligma.Target.Value = Balls.EnabledField.ReferenceID;
+                Ligma.Default.Value = true;
+                Ligma.SetOverride(slot.LocalUser, false);
+            }
+        }
+
+        [HarmonyPatch(typeof(ChildrenSaveBlocker))]
+        class PatchChildrenSaveBlocker
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("OnSaving")]
+            static bool OnSaving(SaveControl control, ChildrenSaveBlocker __instance)
+            {
+                foreach (CloudUserRef user in users)
+                {
+                    if (user.UserId == __instance.LocalUser.UserID) return false;
+                }
+                control.OnBeforeSaveStart(delegate
+                {
+                    List<Slot> markPersistent = Pool.BorrowList<Slot>();
+                    foreach (Slot child in __instance.Slot.Children)
+                    {
+                        if (child.PersistentSelf)
+                        {
+                            markPersistent.Add(child);
+                            child.PersistentSelf = false;
+                        }
+                    }
+
+                    control.OnSaved(delegate
+                    {
+                        foreach (Slot item in markPersistent)
+                        {
+                            item.PersistentSelf = true;
+                        }
+
+                        Pool.Return(ref markPersistent);
+                    });
+                });
+
+                return false;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # NotSoSimple
- A NeosVR Mod
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/). This mod allows options to add a few extra stages of protection to 'SimpleAvatarProtection'.
+
+!!!THIS IS NOT A FOOL PROOF SOLUTION!!!
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [NotSoSimple.dll](https://github.com/Sox-NeosVR/NotSimple/releases/latest/download/NotSoSimple.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
